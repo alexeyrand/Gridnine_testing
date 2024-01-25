67791c78399624f7d83c789d4af1194e67f2a165
«Alexey» <alexey.rand@yandex.ru>
2024-01-26 00:34:02 +0300

    final commit

--- src/main/java/com/gridnine/testing/FilterFlight.java
+++ src/main/java/com/gridnine/testing/FilterFlight.java
@@ -1,25 +1,74 @@
 package com.gridnine.testing;
 
+import javax.swing.plaf.metal.MetalTabbedPaneUI;
 import java.time.LocalDateTime;
 import java.util.List;
 
 public class FilterFlight {
-    public List<Flight> earthFilter(List<Flight> flights) {
-        int i = 1;
-        for (Flight f : flights) {
-            System.out.println("Flight: " + i++);
-            for (Segment s : f.getSegments()) {
-                System.out.println(s);
-                int dateStart = s.getDepartureDate().getHour();
-                int dateEnd = s.getArrivalDate().getHour();
-                int timeInAir = dateEnd - dateStart;
-                if (timeInAir < 0) {
-                    timeInAir = 24 + timeInAir;
-                }
-                System.out.println(timeInAir);
 
+    public void filterOnGround(List<Flight> flights) {
+        int k = 1;
+        for (Flight f : flights) {
+            if (f.getSegments().size() > 1) {
+
+                int allTimeOnGround = 0;
+                for (int i = 0; i < f.getSegments().size() - 1; i++) {
+                    Segment sCurrent = f.getSegments().get(i);
+                    Segment sNext = f.getSegments().get(i + 1);
+
+                    LocalDateTime dateStartWait = sCurrent.getArrivalDate();
+                    LocalDateTime dateEndWait = sNext.getDepartureDate();
+
+                    String timeOnGround = dateEndWait.minusHours(dateStartWait.getHour()).toString()
+                            .split("T")[1].substring(0, 2);
+                    allTimeOnGround += Integer.parseInt(timeOnGround);
+                }
+                if (allTimeOnGround >= 2) {
+                    System.out.println("------------------------ Flight " + k + " ---------------------------");
+                    System.out.println("Время на земле между перелетами: " + allTimeOnGround + " Часа");
+                    System.out.println("Перечень сегментов: " + f);
+                    System.out.println("-----------------------------------------------------------");
+                    System.out.println();
+
+                }
             }
+            k++;
         }
-        return null;
+
+    }
+
+
+    public void filterArrivalBeforeDeparture(List<Flight> flights) {
+        int k = 1;
+        for (Flight f : flights) {
+            for (int i = 0; i < f.getSegments().size(); i++) {
+                Segment s = f.getSegments().get(i);
+                LocalDateTime dateDeparture = s.getDepartureDate();
+                LocalDateTime dateArrival = s.getArrivalDate();
+                if (dateArrival.isBefore(dateDeparture)) {
+                    System.out.println("------------------------ Flight " + k + " ---------------------------");
+                    System.out.println("Перечень сегментов: " + f);
+                    System.out.println("-----------------------------------------------------------");
+                }
+            }
+            k++;
+        }
+    }
+
+
+    public void filterDepartureBefore(List<Flight> flights) {
+        int k = 1;
+        for (Flight f : flights) {
+
+            Segment s = f.getSegments().get(0);
+            if (s.getDepartureDate().isBefore(LocalDateTime.now())) {
+                System.out.println("------------------------ Flight " + k + " ---------------------------");
+                System.out.println("Перечень сегментов: " + f);
+                System.out.println("-----------------------------------------------------------");
+                System.out.println();
+            }
+            k++;
+        }
     }
 }
+
--- src/main/java/com/gridnine/testing/Main.java
+++ src/main/java/com/gridnine/testing/Main.java
@@ -6,8 +6,18 @@ public class Main {
     public static void main(String[] args) {
         FilterFlight filter = new FilterFlight();
         List<Flight> list = FlightBuilder.createFlights();
-        //System.out.println(list);
-        filter.earthFilter(list);
+        System.out.println();
+
+        System.out.println("Перелеты, в которых общее время, проведенное на земле, превышает 2 часа:");
+        filter.filterOnGround(list);
+        System.out.println();
+
+        System.out.println("Перелеты, в которых вылет до текущего момента времени");
+        filter.filterDepartureBefore(list);
+        System.out.println();
+
+        System.out.println("Перелеты, в которых имеются сегменты с датой прилета раньше даты вылета");
+        filter.filterArrivalBeforeDeparture(list);
 
 
 
